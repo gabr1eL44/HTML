acc257c36c76fcf9135cebe8283524ad34ef8307
gabr1eL44 <gabrielinc@gmx.de>
2022-09-01 20:11:06 +0200

    fixes

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <body>
-    <h1>My first PHP page</h1>
-    Hello World!<br/><br/>
-    <?php
-        echo nl2br(min(0, 150, 30, 20, -8, -200) . "\n" . max(0, 150, 30, 20, -8, -200));
-    ?>
+        <h1>My first PHP page</h1>
+        Hello World!<br/><br/>
+        <?php
+            echo nl2br(min(0, 150, 30, 20, -8, -200) . "\n" . max(0, 150, 30, 20, -8, -200));
+        ?>
     </body>
 </html>

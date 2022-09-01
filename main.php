5aaf26c5f6d14de38d8e01b3f73de3827e2b458d
gabr1eL44 <gabrielinc@gmx.de>
2022-09-01 16:18:46 +0200

    php

---
+++ main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <h1>My first PHP page</h1>
+    Hello World!<br/><br/>
+
+    <?php
+        echo nl2br(min(0, 150, 30, 20, -8, -200) . "\n" . max(0, 150, 30, 20, -8, -200));
+    ?>
+    </body>
+</html>

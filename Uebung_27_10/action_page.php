a823a4c97426a0cad4b83ac0b87bbc81d506036f
gabr1eL44 <gabrielinc@gmx.de>
2022-10-28 11:45:10 +0200

    fix

---
+++ Uebung_27_10/action_page.php
@@ -0,0 +1,19 @@
+<?php
+    $anrede = $_GET['anrede'];
+    $vorname = $_GET['vorname'];
+    $nachname = $_GET['nachname'];
+    $strasse = $_GET['strasse'];
+    $hausnummer = $_GET['hausnummer'];
+    $postleitzahl = $_GET['postleitzahl'];
+    $ort_stadt = $_GET['ort_stadt'];
+    $email = $_GET['email'];
+
+    echo "Anrede: $anrede<br/>";
+    echo "Vorname: $vorname<br/>";
+    echo "Nachname: $nachname<br/>";
+    echo "Strasse: $strasse<br/>";
+    echo "Hausnummer: $hausnummer<br/>";
+    echo "Postleitzahl: $postleitzahl<br/>";
+    echo "Ort / Stadt: $ort_stadt<br/>";
+    echo "Email: $email";
+?>

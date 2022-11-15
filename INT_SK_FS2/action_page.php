bab9f74d918abbd2ecce450c4904791c1e5567ad
gabr1eL44 <gabrielinc@gmx.de>
2022-11-15 21:13:19 +0100

    fix

--- INT_SK_FS2/action_page.php
+++ INT_SK_FS2/action_page.php
@@ -1,10 +1,12 @@
 <!-- Dummy-PHP-Skript, um die vom Formular gesendeten Werte zu verarbeiten -->
 <?php
     // Werte aus Formular in Variablen speichern
-    $dummy1 = $_GET['dummy1'];
-    $dummy2 = $_GET['dummy2'];
+    $name= $_GET['name'];
+    $password = $_GET['password'];
+    $email = $_GET['email'];
 
     // Werte / Variablen auf Webseite ausgeben
-    echo "Dummy1: $dummy1<br/>";
-    echo "Dummy2: $dummy2";
+    echo "Name: $name<br/>";
+    echo "Passwort: $password<br/>";
+    echo "E-Mail: $email";
 ?>
